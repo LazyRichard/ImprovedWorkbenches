781de2fe575ca1192c7c966c7f143512b0bb2622
Anuradha Dissanayake <falconne@gmail.com>
2018-09-02 19:27:30 +1200

    fixup! fixup! fixup! fixup! Removing features now in vanilla

--- src/ImprovedWorkbenches/Custom Storage/ExtendedBillData.cs
+++ src/ImprovedWorkbenches/Custom Storage/ExtendedBillData.cs
@@ -7,7 +7,6 @@ namespace ImprovedWorkbenches
 {
     public class ExtendedBillData : IExposable
     {
-        public ThingFilter OutputFilter = new ThingFilter();
         public bool CountAway;
         public string Name;
 
@@ -17,29 +16,15 @@ namespace ImprovedWorkbenches
 
         public void CloneFrom(ExtendedBillData other, bool cloneName)
         {
-            OutputFilter.CopyAllowancesFrom(other.OutputFilter);
             CountAway = other.CountAway;
             if (cloneName)
                 Name = other.Name;
         }
 
-        public void SetDefaultFilter(Bill_Production bill)
-        {
-            var thingDef = bill.recipe.products.First().thingDef;
-            OutputFilter.SetDisallowAll();
-            OutputFilter.SetAllow(thingDef, true);
-        }
-
         public void ExposeData()
         {
-            Scribe_Deep.Look(ref OutputFilter, "outputFilter", new object[0]);
             Scribe_Values.Look(ref CountAway, "countAway", false);
             Scribe_Values.Look(ref Name, "name", null);
-        }
-
-        private static bool IsValidStockpileName(string name)
-        {
-            return !string.IsNullOrEmpty(name) && name != "null";
         }
     }
 }
